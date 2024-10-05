fab14a96be972668b160e1604605fe847304de0a
Ростислав Шведов <rustysquezzy@outlook.com>
2024-10-05 12:59:12 +0300

    The to_float_hours(int hours, int minutes, int seconds) function is implemented

--- BonusTask1/BonusTask1/extra-task-1.cpp
+++ BonusTask1/BonusTask1/extra-task-1.cpp
@@ -1,5 +1,6 @@
 #include "extra-task-1.h"
 
+#include "assert.h"
 
 double seconds_difference(double time_1, double time_2)
 {
@@ -10,4 +11,13 @@ double seconds_difference(double time_1, double time_2)
 double hours_difference(double time_1, double time_2)
 {
     return (time_2 - time_1) / 3600;
+}
+
+
+double to_float_hours(int hours, int minutes, int seconds)
+{
+    assert(seconds >= 0 && seconds < 60);
+    assert(minutes >= 0 && minutes < 60);
+    
+    return seconds / 3600.0 + minutes / 60.0 + hours;
 }
